841644e8bdc3fd434f8fefd99d13d4c2c6a0a9ac
Sebastian Pietrulewicz <pietrulewicz.sebastian@gmail.com>
2015-01-18 18:13:24 +0100

    Almost final

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :as => :commentable 
-  has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>", :show => "700x700>"}, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>", :show => "1000x1000>"}, :default_url => "/images/:styles/missing.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,9 +4,15 @@ RailsAdmin.config do |config|
 
   
    config.authenticate_with do
-     warden.authenticate! scope: :user#, ->{where (admin: true) }
+     warden.authenticate! scope: :user
    end
    config.current_user_method(&:current_user)
+
+  config.authorize_with do
+    redirect_to main_app.root_path unless current_user.admin?
+  end
+
+
 
   ## == Cancan ==
   # config.authorize_with :cancan

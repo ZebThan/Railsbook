eaf02ad3b774ba243bd90982d7c523d350e4a80c
Sebastian Pietrulewicz <pietrulewicz.sebastian@gmail.com>
2015-02-28 14:17:38 +0100

    minor fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :comments, :as => :commentable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "images/:style/missing.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 validates :name, :length => {:in => 3..30}
 validates :age, :numericality => { :only_integer => true, :less_than => 100 } 
